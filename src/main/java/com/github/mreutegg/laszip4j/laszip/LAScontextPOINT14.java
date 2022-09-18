22cd3942e405b95bf72cae91b099a699e14d0f59
Jason Daly <jason_daly@trimble.com>
2022-09-19 09:47:59 +1200

    Resolves #15; adds COPYRIGHT; removes replaced wavepacket files

--- src/main/java/com/github/mreutegg/laszip4j/laszip/LAScontextPOINT14.java
+++ src/main/java/com/github/mreutegg/laszip4j/laszip/LAScontextPOINT14.java
@@ -1,3 +1,15 @@
+/*
+  COPYRIGHT:
+
+    (c) 2007-2022, rapidlasso GmbH - fast tools to catch reality
+
+    This is free software; you can redistribute and/or modify it under the
+    terms of the Apache Public License 2.0 published by the Apache Software
+    Foundation. See the COPYING file for more information.
+
+    This software is distributed WITHOUT ANY WARRANTY and without even the
+    implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
 package com.github.mreutegg.laszip4j.laszip;
 
 class LAScontextPOINT14
